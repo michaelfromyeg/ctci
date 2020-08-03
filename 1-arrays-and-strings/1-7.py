cb9f06307e9d65ce939bf3a960168959334ee5fb
Michael DeMarco <michaelfromyeg@gmail.com>
2020-08-03 10:28:18 -0600

    Finished 1-7, 1-8

--- 1-arrays-and-strings/1-7.py
+++ 1-arrays-and-strings/1-7.py
@@ -2,29 +2,20 @@
 # TODO: do this in place
 
 from array import *
+import itertools
 
 def rotate_matrix(m: [[]]) -> [[]]: # Not sure if it's possible to enforce 2D int array
   if (len(m) <= 1):
     return m
-  new = [[0]*len(m)]*len(m)
-  print('1 ====')
+  
+  # Initialize a new matrix
+  temp = itertools.count(1)  
+  new = [[next(temp) for i in range(len(m))] for i in range(len(m))] # create outer arrays of inner arrays
+
   for i, r in enumerate(m): # for each row
     for j, c in enumerate(r): # for each cell
       new[j][len(m) - 1 - i] = c
-      print(j,len(m) - 1 - i, new[j][len(m) - 1 - i])
-  print('2 ====')
-  for i, r in enumerate(new): # for each row
-    for j, c in enumerate(r): # for each cell
-      print(i,j,new[i][j])
   return new
 
-def check(m: [[]], n: [[]]) -> bool:
-  for i, r in enumerate(m): # for each row
-    for j, c in enumerate(r): # for each cell
-      if (m[i][j] != n[i][j]):
-        print(i,j, m[i][j], n[i][j])
-        return False
-  return True
-
-assert check(rotate_matrix([[]]), [[]]) == True
-assert check(rotate_matrix([[0,1,2],[3,4,5],[6,7,8]]), [[6,3,0],[7,4,1],[8,5,2]]) == True 
+assert rotate_matrix([[]]) == [[]]
+assert rotate_matrix([[0,1,2],[3,4,5],[6,7,8]]) == [[6,3,0],[7,4,1],[8,5,2]]
--- 1-arrays-and-strings/1-8.py
+++ 1-arrays-and-strings/1-8.py
@@ -1,9 +1,35 @@
-# Rotate Matrix: rotate an NxN matrix by 90 deg
-# TODO: do this in place
+# Zero Matrix: if a value in an NxM matrix is 0, it's row/col becomes 0
 
 from array import *
 
-def rotate_matrix(m: int[[]]) -> int[[]]:
-  return [[]]
+def zero_matrix(m: [[]]) -> [[]]:
+  zeros = [] # a list of (x,y) tuples
+  # Find all 0s as x,y pairs
+  for i, r in enumerate(m):
+    for j, c in enumerate(r):
+      if (c == 0):
+        zeros.append((i,j))
+  # Handle tuples
+  for val in zeros:
+    x = val[0]
+    y = val[1]
+    for i in range(0, len(m)): # dealing with rows
+      m[i][y] = 0
+    for j in range(0, len(m[0])): # dealing with cols
+      m[x][j] = 0
+  return m
 
-assert rotate_matrix([[]]) == [[]]
+# Empty test
+assert zero_matrix([[]]) == [[]]
+
+# Single 0 -- outside
+assert zero_matrix([[0,1,2],[3,4,5],[6,7,8]]) == [[0,0,0],[0,4,5],[0,7,8]]
+
+# Single 0 -- middle
+assert zero_matrix([[1,2,3],[4,0,6],[7,8,9]]) == [[1,0,3],[0,0,0],[7,0,9]]
+
+# Two 0
+assert zero_matrix([[0,1,2],[3,4,5],[6,7,0]]) == [[0,0,0],[0,4,0],[0,0,0]]
+
+# Three 0
+assert zero_matrix([[0,1,2],[3,0,5],[6,7,0]]) == [[0,0,0],[0,0,0],[0,0,0]]
